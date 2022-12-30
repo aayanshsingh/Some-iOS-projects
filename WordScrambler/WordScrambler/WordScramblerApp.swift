b793642804182e8b0f5c49cffed3451d4dbac59e
RegulusSpica1 <51476865+aayanshsingh@users.noreply.github.com>
2022-12-30 21:22:32 +0530

    Add files via upload

---
+++ WordScrambler/WordScrambler/WordScramblerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScramblerApp.swift
+//  WordScrambler
+//
+//  Created by aayansh singh on 30/12/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScramblerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
